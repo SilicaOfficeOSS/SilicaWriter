6ffc3c9c08128e37b9bfe36743f3e81b456948f7
jpb <jpb44173@gmail.com>
2026-05-23 10:16:53 +0100

    Code cleanup
    Remove unnecessary imports and methods

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,16 +1,8 @@
 ﻿using Fluent;
-using iNKORE.UI.WPF.Converters;
-using iNKORE.UI.WPF.Helpers;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SilicaWriter
 {
@@ -59,11 +51,6 @@ namespace SilicaWriter
             {
                 editor.FontFamily = selectedFontFamily;
             }
-        }
-
-        private void Underline(object sender, RoutedEventArgs e)
-        {
-            editor.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, TextDecorations.Underline);
         }
 
         void UpdateItemCheckedState(ToggleButton button, DependencyProperty formattingProperty, object expectedValue)
